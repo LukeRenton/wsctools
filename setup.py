55eef09ba3c153b84c36a621c773d42d6b704c84
Luke Renton <luke.renton@icloud.com>
2025-01-27 18:26:39 +0200

    PyPI documentation finalisation

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup
 
+with open("READEME.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name='wsctools',
-    version='1.0.0',
+    version='1.0.1',
+    author='Luke Renton',
+    description='A collection of helper functions for web scraping',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/LukeRenton/wsctools',
     packages=['wsctools'],
-     install_requires=[
+    install_requires=[
         'googletrans==4.0.0-rc1',
         'langdetect==1.0.9',
         'tldextract==5.1.1',
     ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+
 )
